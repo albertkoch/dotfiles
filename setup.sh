1c2e49ce184df79dcb9838d40eb3c7f7a62df3a3
Albert Koch <kocha@slagit.net>
2013-10-29 12:15:01 -0400

    Convert shell scripts to /bin/sh instead of /bin/bash

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
-SCRIPT=$(readlink -f "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT")
-TPUT=$(which tput)
+SCRIPT=`readlink -f "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT"`
+TPUT=`which tput`
 FILES="Xmodmap ratpoisonrc screenrc vim vimrc"
 EXECUTABLES="ratpoison screen wmname xset xsetroot xterm xtrlock"
 
-if [[ $(pwd) != "${HOME}" ]]; then
-    echo "WARNING: This script is meant to be run from your home directory, but you have run it from $(pwd)." | fmt
+if [ "${PWD}" != "${HOME}" ]; then
+    echo "WARNING: This script is meant to be run from your home directory, but you have run it from ${PWD}." | fmt
     echo
-    read -p "Press [Ctrl-C] to quit or [Enter] to ignore this warning... "
+    read -p "Press [Ctrl-C] to quit or [Enter] to ignore this warning... " throwaway
 fi
 
-function colorize() {
+colorize() {
     if [ ! -z "${TPUT}" ]; then
         tput "$@"
     fi
 }
 
-function relpath() {
+relpath() {
     python -c "import os.path; print os.path.relpath('$1','$2')"
 }
 
@@ -26,38 +26,38 @@ for exe in ${EXECUTABLES}; do
     printf "Validating presence of %-40s" "${exe} ..."
     location=`which ${exe}`
     if [ 0 -eq $? ]; then
-        printf "%s%s%s\n" "$(colorize setaf 2)" "$location" "$(colorize sgr0)"
+        printf "%s%s%s\n" "`colorize setaf 2`" "$location" "`colorize sgr0`"
     else
-        echo -e "$(colorize setaf 1)not found$(colorize sgr0)"
+        echo "`colorize setaf 1`not found`colorize sgr0`"
     fi
 done
 
 for file in ${FILES}; do
-    link_name="$(pwd)/.$(basename $file)"
-    target=$(relpath "${SCRIPT_DIR}/${file}" "$(pwd)")
+    link_name="${PWD}/.`basename $file`"
+    target=`relpath "${SCRIPT_DIR}/${file}" "${PWD}"`
 
     if [ -e "${link_name}" -o -h "${link_name}" ]; then
-        if [[ "$(readlink ${link_name})" != "$target" ]]; then
-            echo "$(colorize setaf 1)WARNING: ${link_name} exists, but is not a symbolic link to ${target}$(colorize sgr0)"
+        if [ "`readlink ${link_name}`" != "$target" ]; then
+            echo "`colorize setaf 1`WARNING: ${link_name} exists, but is not a symbolic link to ${target}`colorize sgr0`"
         fi
     else
         printf "Linking %-55s" "${link_name} -> ${target}..."
-        link_out=$(ln -s "${target}" "${link_name}" 2>&1)
-        if [[ 0 == $? ]]; then
-            echo "$(colorize setaf 2)success$(colorize sgr0)"
+        link_out=`ln -s "${target}" "${link_name}" 2>&1`
+        if [ 0 -eq $? ]; then
+            echo "`colorize setaf 2`success`colorize sgr0`"
         else
-            echo "$(colorize setaf 1)failure$(colorize sgr0)"
+            echo "`colorize setaf 1`failure`colorize sgr0`"
             echo $link_out
         fi
     fi
 done
 
-bashrc="$(relpath "${SCRIPT_DIR}/bashrc" "$(pwd)")"
+bashrc="`relpath "${SCRIPT_DIR}/bashrc" "${PWD}"`"
 bashrc="source ${bashrc} ${bashrc}"
 if ! (grep "$bashrc" .bashrc 2>&1 >/dev/null); then
     printf "Adding configuration to .bashrc...                             "
     echo $bashrc >> .bashrc
-    echo "$(colorize setaf 2)done$(colorize sgr0)"
+    echo "`colorize setaf 2`done`colorize sgr0`"
 fi
 
 # vim:tabstop=4:shiftwidth=4:expandtab
--- update.sh
+++ update.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -f "$0")
-SCRIPT_DIR=$(dirname "$SCRIPT")
+SCRIPT=`readlink -f "$0"`
+SCRIPT_DIR=`dirname "$SCRIPT"`
 
 cd "${SCRIPT_DIR}"
 
-current_branch="$(expr $(git symbolic-ref HEAD) : 'refs/heads/\(.*\)')"
-current_rev="$(git rev-list --max-count=1 ${current_branch})"
+symref="`git symbolic-ref HEAD`"
+current_branch="`expr ${symref} : 'refs/heads/\(.*\)'`"
+current_rev="`git rev-list --max-count=1 ${current_branch}`"
 
-remote="$(git config branch.${current_branch}.remote)"
-remote_ref="$(git config branch.${current_branch}.merge)"
-remote_branch="$(expr $remote_ref : 'refs/heads/\(.*\)')"
+remote="`git config branch.${current_branch}.remote`"
+remote_ref="`git config branch.${current_branch}.merge`"
+remote_branch="`expr $remote_ref : 'refs/heads/\(.*\)'`"
 tracking_branch="refs/remotes/${remote}/${remote_branch}"
-tracking_rev="$(git rev-list --max-count=1 ${tracking_branch})"
+tracking_rev="`git rev-list --max-count=1 ${tracking_branch}`"
 
 if [ "${current_rev}" != "${tracking_rev}" ]; then
-    ancestor_rev="$(git merge-base ${current_rev} ${tracking_rev})"
+    ancestor_rev="`git merge-base ${current_rev} ${tracking_rev}`"
     if [ "${ancestor_rev}" = "${current_rev}" ]; then
         echo -n "(NEED PULL) "
     elif [ "${ancestor_rev}" = "${tracking_rev}" ]; then
@@ -25,7 +26,7 @@ if [ "${current_rev}" != "${tracking_rev}" ]; then
     fi
 fi
 
-untracked_files="$(git ls-files --other --exclude-standard --directory)"
+untracked_files="`git ls-files --other --exclude-standard --directory`"
 git diff --exit-code >/dev/null 2>/dev/null
 need_add=$?
 git diff --cached --exit-code >/dev/null 2>/dev/null
@@ -35,3 +36,5 @@ if [ ! -z "${untracked_files}" -o  0 != ${need_add} -o 0 != ${need_commit} ]; th
     echo -n "(NEED COMMIT) "
 fi
 echo
+
+# vim:tabstop=4:shiftwidth=4:expandtab
