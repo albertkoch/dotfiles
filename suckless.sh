8c47dea01c1eeae9062d58a667f3e308fbd3fbd2
Rob Paisley <paisley@rpaisley.com>
2013-11-06 21:13:12 -0500

    Check all programs listed and patch wmname for BSD
    Added checks for programs tic and wget
    Patch wmname for compilation on FreeBSD

--- suckless.sh
+++ suckless.sh
@@ -10,7 +10,7 @@ sl_install()
     wget "$3" -O "$target".tar.gz
   fi
   cd "$HOME"/tool/build
-  gzip -dc "$HOME"/tool/src/"$tfile" | tar -tf -
+  gzip -dc "$HOME"/tool/src/"$tfile" | tar -xf -
   cd "$target"
   if [ -e st.c ]; then
     patch -r - -p0 << EOT
@@ -26,6 +26,25 @@ sl_install()
  static char shell[] = "/bin/sh";
 
 EOT
+  else if [ -e wmname.c ]; then
+    if [ -d /usr/local/include/X11 ]; then
+      patch -r - -p0 << EOT
+--- config.mk	2008-08-02 14:36:10.000000000 -0400
++++ config.mk.new	2013-11-06 20:42:20.000000000 -0500
+@@ -7,8 +7,8 @@
+ PREFIX = /usr/local
+ MANPREFIX = ${PREFIX}/share/man
+ 
+-X11INC = /usr/X11R6/include
+-X11LIB = /usr/X11R6/lib
++X11INC = /usr/local/include
++X11LIB = /usr/local/lib
+ 
+ # includes and libs
+ INCS = -I. -I/usr/include -I${X11INC}
+EOT
+      fi
+    fi
   fi
   make PREFIX="$HOME"/tool/install/"$target" install
   cd "$HOME"/tool/build
@@ -34,11 +53,14 @@ EOT
   stow "$target"
 }
 
-if ! which stow > /dev/null 2>&1; then
-  printf "Unable to locate stow\n" 1>&2
-  exit 1
-fi
+for p in stow tic wget; do
+  if ! which "$p" > /dev/null; then
+    printf "Unable to locate %s\n" "$p" 1>&2
+    exit 1
+  fi
+done
 
+cd "$HOME"
 mkdir -p tool/build tool/install tool/src
 sl_install wmname 0.1 http://dl.suckless.org/tools/wmname-0.1.tar.gz
 sl_install st 0.4.1 http://dl.suckless.org/st/st-0.4.1.tar.gz
